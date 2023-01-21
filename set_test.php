1875a0b42611e66cc4098513046c9c067bc99993
sreeventh <sreeventhsrinivasan@gmail.com>
2023-01-21 15:47:00 +0530

    test dur check

--- set_test.php
+++ set_test.php
@@ -17,7 +17,7 @@ if(isset($_POST["create"])){
     $dup_name = $_POST['tname'];
     $dupset_1 = mysqli_query($con , "select * from tcat where tname='$dup_name' ") or die(mysqli_error($con));
     $dcount = mysqli_num_rows($dupset_1);
-   if($dcount == 0)
+   if($dcount == 0 and $_POST['ttime']>0)
     {
         $tdata = "insert into tcat values (NULL , '$_POST[tname]' , '$_POST[ttime]') ";
     $update_result = mysqli_query( $con , $tdata ) or die ('Unable to execute query. '. mysqli_error($con));
@@ -29,13 +29,21 @@ if(isset($_POST["create"])){
     <?php
        
     }
-    else{
+    else if($dcount!=0){
        ?>
        <script>
         alert("Test Already Present!...Create a New One Please")
         window.location.href = window.location.href
        </script>
        <?php
+    }
+    else{
+        ?>
+        <script>
+            alert("Test Duration Invalid!")
+            window.location.href = window.location.href
+        </script>
+        <?php
     }
     }
     
@@ -129,7 +137,7 @@ if(isset($_POST["create"])){
 
             <div id="ttable" class="container-fluid">
                 <div class="card">
-                    <div class="card-header">
+                    <div class="card-header" style="text-align: center;">
                         <strong class="card-title">Current Tests</strong>
                     </div>
 
